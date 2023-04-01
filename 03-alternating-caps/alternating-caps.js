4b4134a43ca8d6cae47ca59c641a0ba84309d885
Xavi Guasch <xaviguasch@gmail.com>
2023-04-01 15:29:26 +0200

    added solution to previous challenge

--- 03-alternating-caps/alternating-caps.js
+++ 03-alternating-caps/alternating-caps.js
@@ -7,7 +7,17 @@ Example output: "I'M So hApPy iT'S MoNdAy"
 */
 
 const altCaps = (str) => {
-  return
+  let newStr = ''
+
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0) {
+      newStr += str[i].toUpperCase()
+    } else {
+      newStr += str[i]
+    }
+  }
+  return newStr
 }
 
 console.log(altCaps('When you visit Portland you have to go to VooDoo Donuts'))
+console.log(altCaps("I'm so happy it's Monday"))
