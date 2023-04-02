d0be942670c5997f3183fef4c822e27858480049
Xavi Guasch <xaviguasch@gmail.com>
2023-04-02 12:59:41 +0200

    added solution to previous challenge

--- 07-is-it-anagram/is-it-anagram.js
+++ 07-is-it-anagram/is-it-anagram.js
@@ -15,7 +15,21 @@ Example output: false
 
 */
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  // are the strings the same length? if yes return false.
+  if (str1.length !== str2.length) return false
+
+  // split string into an array
+  // sort
+  // join the array back together as a string
+  const newStr1 = str1.split('').sort().join('')
+
+  // repeat with second word
+  const newStr2 = str2.split('').sort().join('')
+
+  // are the two words equal? true or false
+  return newStr1 === newStr2
+}
 
 console.log(isAnagram('allergy', 'gallery'))
 console.log(isAnagram('treasure', 'measure'))
