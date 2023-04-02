b6a88e54b88f5a66d520005a18546d3920a59e02
Xavi Guasch <xaviguasch@gmail.com>
2023-04-02 12:44:45 +0200

    added solution to previous challenge

--- 06-emojis/emojis.js
+++ 06-emojis/emojis.js
@@ -36,8 +36,20 @@ Example input: "elephant"
 Example output: "elephant"
 */
 
-const emojifyWord = (word) => {
-  return
+function emojifyWord(word) {
+  // starts or ends with colon?
+  // no: return word
+  if (!word.startsWith(':') && !word.endsWith(':')) return word
+
+  // yes: remove colons
+  const slice = word.slice(1, -1)
+  // exists in emoji object?
+  if (emojis[slice]) {
+    // yes: return emoji
+    return emojis[slice]
+  } else {
+    return slice
+  }
 }
 
 /* 2. Write a function to find any emoji shortcodes in a phrase.
@@ -52,13 +64,17 @@ Example input: "I :heart: my elephant"
 Example output: "I 💜 my elephant"
 */
 
-const emojifyPhrase = (phrase) => {
-  return
+function emojifyPhrase(phrase) {
+  // split the passed in phrase into an array
+  // map through the array and call emojifyWord() on each word in the phrase
+  // join the array back together as a string, and return
+  const newPhrase = phrase.split(' ').map((word) => emojifyWord(word))
+  return newPhrase.join(' ')
 }
 
 // console.log(emojifyWord(":heart:"));
 // console.log(emojifyWord(":flower:"));
 // console.log(emojifyWord("elephant"));
 
-// console.log(emojifyPhrase("I :heart: my :cat:"));
-// console.log(emojifyPhrase("I :heart: my :elephant:"));
+console.log(emojifyPhrase('I :heart: my :cat:'))
+console.log(emojifyPhrase('I :heart: my :elephant:'))
