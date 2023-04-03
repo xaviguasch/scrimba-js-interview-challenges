e9f0137763dcd52d28a15ec77e6ed1eb5e03e2ff
Xavi Guasch <xaviguasch@gmail.com>
2023-04-03 10:36:55 +0200

    added solution to previous challenge

--- 14-count-the-scrimba-students/count-the-scrimba-students.js
+++ 14-count-the-scrimba-students/count-the-scrimba-students.js
@@ -14,7 +14,7 @@ Example output: 6
 const studentCount = [50, 53, 61, 67, 60, 70, 78, 80, 80, 81, 90, 110]
 
 function sumArray(arr) {
-  return
+  return arr.reduce((acc, currV) => acc + currV, 0)
 }
 
 console.log(sumArray(studentCount))
