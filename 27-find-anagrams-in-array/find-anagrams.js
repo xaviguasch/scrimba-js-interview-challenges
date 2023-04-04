a9a243e3601381584e23a89f9cdc5fba259600dd
Xavi Guasch <xaviguasch@gmail.com>
2023-04-04 20:47:33 +0200

    added solution to previous challenge

--- 27-find-anagrams-in-array/find-anagrams.js
+++ 27-find-anagrams-in-array/find-anagrams.js
@@ -1,22 +1,12 @@
-/* Find anagrams in an array   
+// Your function should take two parameters: the phrase you want to compare to
+// the anagrams, and an array of anagrams. The function should return
+// a new array of anagrams that match the phrase.
 
-When two words have the exact same letters, they are anagrams. 
+// Example input: treat, ["tater", "tree", "teart", "tetra", "heart", "hamster"]
+// Example output: ["tater", "teart", "tetra"]
 
-Each item in the anagrams array is an anagram of a Scrimba teacher's
-first and last name, plus the phrase "Scrimba teacher". 
-
-Write a function to determine which strings in the array are 
-anagrams of "Bob Ziroll Scrimba Teacher".
-
-Your function should take two parameters: the phrase you want to compare to
-the anagrams, and an array of anagrams. The function should return
-a new array of anagrams that match the phrase. 
-
-Example input: treat, ["tater", "tree", "teart", "tetra", "heart", "hamster"]
-Example output: ["tater", "teart", "tetra"]
-
-Bonus: What other teachers are represented in these anagrams? 
- */
+// Bonus: What other teachers are represented in these anagrams?
+//  */
 
 const anagrams = [
   'moz biblical torchbearers',
@@ -36,8 +26,23 @@ const anagrams = [
   'bam ratchet mechatronics',
 ]
 
+function sortPhrase(phrase) {
+  return phrase.toLowerCase().split('').sort().join('').trim()
+}
+
 function isAnagramInArray(anagram, arr) {
-  return
+  // use .filter()
+  // use sortPhrase() to sort both the input phrase and the current phrase in the array
+  // compare the two words to return true or false
+  return arr.filter((item) => {
+    const word1 = sortPhrase(anagram)
+    const word2 = sortPhrase(item)
+
+    return word1 === word2
+  })
 }
 
 console.log(isAnagramInArray('Bob Ziroll Scrimba Teacher', anagrams))
+console.log(
+  isAnagramInArray('treat', ['tater', 'tree', 'teart', 'tetra', 'heart', 'hamster'])
+)
